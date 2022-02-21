baa326dbaedec350e3d1b7208eb7329c279c66c1
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2022-02-21 16:57:51 +0100

    Add project files.

---
+++ OOPCalculator/Model/Calc.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPCalculator
+{
+    class Calc
+    {
+        public double Addition(double numberOne,double numberTwo)
+        {
+            return numberOne + numberTwo;
+
+        }
+        public double Subtraction(double numberOne,double numberTwo)
+        {
+            return numberOne - numberTwo;          
+
+        }
+        public double Multiplication(double numberOne, double numberTwo)
+        {
+            return numberOne * numberTwo;
+        }
+        
+        public double Division(double numberOne,double numberTwo)
+        {
+            if (numberTwo==0)
+            {
+                throw new DivideByZeroException("You can't divide a number with zero");
+            }
+            return numberOne / numberTwo;
+
+        }
+public double Addition(double[] numbers)
+        {
+            double sum = 0;
+            foreach (double term in numbers)
+            {
+                sum = Addition(sum, term);
+            }
+            return sum;
+        }
+        public double Subtraction(double[] numbers)
+        {
+            double diff = 0;
+            foreach (double term in numbers)
+            {
+                diff = Subtraction(diff, term);
+            }
+            return diff;
+        }
+
+    }
+}
---
+++ OOPCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace OOPCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calculator = new Calc();
+
+            Console.WriteLine("{0}", calculator.Addition(new double[] { 1.1, -3.3, 5, 7, 8.8 }));
+            Console.WriteLine("{0}", calculator.Addition(1.1, -3.3));
+            Console.WriteLine("{0}", calculator.Division(1.1, 0));
+
+        }
+    }
+}
